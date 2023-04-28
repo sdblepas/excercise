f0794721b0f2699eb22385c226556f8185f05432
Benjamin Elharrar <benjamin@browzwear.com>
2023-04-28 14:44:35 +0300

    add admin user to the db

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
-import datetime
-
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
